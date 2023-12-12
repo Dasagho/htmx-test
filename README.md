cd920d0fafa017a5428999741dcd4a3c4cf627f1
David <davidamir03@gmail.com>
2023-12-12 19:42:15 +0000

    logging system

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # To Do
-- Logging system
-- Dockerize app
 - Database ORM
--- handlers/index.go
+++ handlers/index.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	logging "github.com/dasagho/htmx-test/log"
 	"github.com/dasagho/htmx-test/models"
 	"github.com/dasagho/htmx-test/views"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	logging.Info("Index Requested")
 	data := models.Body{
 		MouseDiv: "Mamala",
 		Search:   "Search",
@@ -19,6 +21,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	err := views.GetTemplates().ExecuteTemplate(w, "index", data)
 	if err != nil {
 		fmt.Fprintf(w, "error al ejecutar el template index %s", err)
+		return
 	}
-
+	logging.Info("Index correct attended")
 }
