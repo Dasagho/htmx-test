93cf35bfa9e04dc573accc1a5b53a0bbd6efd963
david <damirsa2001@gmail.com>
2023-12-17 13:07:02 +0000

    improve error handle db connection

--- db/main.go
+++ db/main.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,37 +10,41 @@ import (
 	"time"
 
 	logging "github.com/dasagho/htmx-test/log"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
-var err error
+var (
+	db          *sql.DB
+	err         error
+	credentials = map[string]string{
+		"DB_HOST": "",
+		"DB_PORT": "",
+		"DB_USER": "",
+		"DB_PASS": "",
+		"DB_NAME": "",
+		"DB_SSL":  "",
+	}
 
-var credentials = map[string]string{
-	"DB_HOST": "",
-	"DB_PORT": "",
-	"DB_USER": "",
-	"DB_PASS": "",
-	"DB_NAME": "",
-	"DB_SSL":  "",
-}
+	defaultCredentials = map[string]string{
+		"DB_HOST": "localhost",
+		"DB_PORT": "5432",
+		"DB_USER": "postgres",
+		"DB_PASS": "root",
+		"DB_NAME": "database_test",
+		"DB_SSL":  "disable",
+	}
 
-var defaultCredentials = map[string]string{
-	"DB_HOST": "localhost",
-	"DB_PORT": "5432",
-	"DB_USER": "postgres",
-	"DB_PASS": "root",
-	"DB_NAME": "database_test",
-	"DB_SSL":  "disable",
-}
+	errDBAlreadyExists = errors.New("error database already exists")
+)
 
 func ConnectDB() {
 	initializeCredentials()
 
 	// Connect to Database
 	connectionCredentials := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=%s`,
-	credentials["DB_HOST"], credentials["DB_PORT"], credentials["DB_USER"],
-	credentials["DB_PASS"], "postgres", credentials["DB_SSL"])
+		credentials["DB_HOST"], credentials["DB_PORT"], credentials["DB_USER"],
+		credentials["DB_PASS"], "postgres", credentials["DB_SSL"])
 
 	err = connectWithRetry(connectionCredentials, 5)
 
@@ -55,7 +60,7 @@ func ConnectDB() {
 
 	// Ejecutar script para crear la base de datos y cerrar conexion
 	err = executeSQLFile(db, filepath.Join("db", "migrations", "0-createDataBase.sql"))
-	if err != nil {
+	if err != nil && !errors.Is(err, errDBAlreadyExists) {
 		logging.Error("Failed to execute migration 0")
 		log.Println(err)
 	}
@@ -89,28 +94,28 @@ func ConnectDB() {
 }
 
 func connectWithRetry(dbURL string, maxAttempts int) error {
-    var err error
+	var err error
 
-    for attempts := 0; attempts < maxAttempts; attempts++ {
-        db, err = sql.Open("postgres", dbURL)
-        if err != nil {
+	for attempts := 0; attempts < maxAttempts; attempts++ {
+		db, err = sql.Open("postgres", dbURL)
+		if err != nil {
 			logging.Error("No se pudo conectar a la base de datos: " + err.Error() + ". Reintentando...")
-            log.Printf("No se pudo conectar a la base de datos: %v. Reintentando...", err)
-        } else {
-            err = db.Ping()
-            if err == nil {
+			log.Printf("No se pudo conectar a la base de datos: %v. Reintentando...", err)
+		} else {
+			err = db.Ping()
+			if err == nil {
 				logging.Debug("Conexión a la base de datos establecida con éxito.")
-                log.Println("Conexión a la base de datos establecida con éxito.")
-                return nil
-            }
-            log.Printf("Error al realizar ping a la base de datos: %v. Reintentando...", err)
-        }
+				log.Println("Conexión a la base de datos establecida con éxito.")
+				return nil
+			}
+			log.Printf("Error al realizar ping a la base de datos: %v. Reintentando...", err)
+		}
 
-        // Esperar antes del próximo reintento
-        time.Sleep(2 * time.Second)
-    }
+		// Esperar antes del próximo reintento
+		time.Sleep(2 * time.Second)
+	}
 
-    return fmt.Errorf("después de %d intentos, no se pudo conectar a la base de datos: %v", maxAttempts, err)
+	return fmt.Errorf("después de %d intentos, no se pudo conectar a la base de datos: %v", maxAttempts, err)
 }
 
 func initializeCredentials() {
@@ -133,8 +138,17 @@ func executeSQLFile(db *sql.DB, filePath string) error {
 
 	_, err = db.Exec(string(content))
 	if err != nil {
-		log.Printf("Error ejecutando el archivo SQL (%s): %v", filePath, err)
-		return err
+		if pqErr, ok := err.(*pq.Error); ok {
+			// Verificar el código de error específico de PostgreSQL
+			if pqErr.Code == "42P04" {
+				// Manejar el error específico aquí
+				logging.Debug("Database already Exists")
+				return errDBAlreadyExists
+			}
+		} else {
+			log.Printf("Error ejecutando el archivo SQL (%s): %v", filePath, err)
+			return err
+		}
 	}
 	return nil
 }
